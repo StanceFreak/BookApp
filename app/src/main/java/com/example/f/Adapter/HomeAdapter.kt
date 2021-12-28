26efd45d53cb26801998e244d51ab4e55dbd0022
StanceFreak <jombor.army67@gmail.com>
2021-12-28 18:27:55 +0800

    Stuck on searchView filter

--- app/src/main/java/com/example/f/Adapter/HomeAdapter.kt
+++ app/src/main/java/com/example/f/Adapter/HomeAdapter.kt
@@ -1,21 +1,25 @@
 package com.example.f.Adapter
 
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
+import android.widget.*
+import androidx.compose.ui.text.toLowerCase
 import androidx.recyclerview.widget.RecyclerView
 import com.example.f.Activity.ItemDetailActivity
 import com.example.f.ModelRomance.Item
 import com.example.f.R
 import com.squareup.picasso.Picasso
+import java.util.*
+import kotlin.collections.ArrayList
 
-class HomeAdapter: RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+class HomeAdapter: RecyclerView.Adapter<HomeAdapter.HomeViewHolder>(), Filterable {
 
-    private var data : List<Item>? = listOf()
+    private var filteredData = ArrayList<Item>()
+    private var data = ArrayList<Item> ()
+
 
     inner class HomeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var image: ImageView
@@ -30,16 +34,18 @@ class HomeAdapter: RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        return HomeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.romance_recycler, parent, false))
+        return HomeViewHolder(LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.romance_recycler, parent, false))
 
     }
 
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
-        val data = data!!.get(position)
-        holder.title.text = data.volumeInfo.title
+        val data = filteredData.get(position)
+        holder.title.text = data.volumeInfo?.title
 
         @Suppress("ConstantConditionIf")
-        if(data.volumeInfo.imageLinks != null) {
+        if(data.volumeInfo?.imageLinks != null) {
             Picasso.get()
                 .load(data.volumeInfo.imageLinks.thumbnail)
                 .error(R.drawable.ic_no_thumbnail)
@@ -49,17 +55,50 @@ class HomeAdapter: RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
 
         holder.itemView.setOnClickListener{
             val i = Intent(holder.itemView.context, ItemDetailActivity::class.java)
-            i.putExtra(ItemDetailActivity.BOOK_DETAIL, data.id)
+            i.putExtra(ItemDetailActivity.BOOK_DETAIL, data)
             holder.itemView.context.startActivity(i)
         }
     }
 
-    override fun getItemCount(): Int {
-        return data!!.size
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(charSequence: CharSequence?): FilterResults {
+                val queryString = charSequence.toString()
+
+                if (queryString.isEmpty()) {
+                    filteredData = data
+                }
+                else {
+                    val tempFilteredData = ArrayList<Item>()
+
+                    for (item in data) {
+                        if (item.volumeInfo?.title?.toLowerCase(Locale.ROOT)!!.contains(queryString.toLowerCase(Locale.ROOT))) {
+                            tempFilteredData.add(item)
+                        }
+                    }
+                    filteredData = tempFilteredData
+                }
+
+                val filterResults = FilterResults()
+                filterResults.values = filteredData
+                return filterResults
+            }
+
+            override fun publishResults(charSequence: CharSequence?, filterResults: FilterResults?) {
+                filteredData = filterResults?.values as ArrayList<Item>
+                notifyDataSetChanged()
+            }
+
+        }
     }
 
-    fun setData(bookList: List<Item>) {
+    override fun getItemCount(): Int {
+        return filteredData.size
+    }
+
+    fun setData(bookList: ArrayList<Item>) {
         this.data = bookList
+        filteredData = data
         notifyDataSetChanged()
     }
 
--- app/src/main/java/com/example/f/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/f/Fragment/HomeFragment.kt
@@ -1,41 +1,40 @@
-package com.example.f.Fragment
+ package com.example.f.Fragment
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.f.Adapter.HomeAdapter
 import com.example.f.Api.BookClient
+import com.example.f.ModelRomance.Item
 import com.example.f.ModelRomance.RomanceBooks
 import com.example.f.R
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class HomeFragment : Fragment() {
+ class HomeFragment : Fragment(){
 
     private lateinit var homeAdapter: HomeAdapter
-
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        val toolbar = view.findViewById<Toolbar>(R.id.home_toolbar)
         val recyclerView = view.findViewById<RecyclerView>(R.id.home_rv_romance)
         homeAdapter = HomeAdapter()
 
@@ -44,11 +43,50 @@ class HomeFragment : Fragment() {
             adapter = homeAdapter
             setHasFixedSize(true)
         }
+
+        toolbar.inflateMenu(R.menu.menu_home)
+        toolbar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.tb_search -> Toast.makeText(context,
+                        "Clicked search button",
+                        Toast.LENGTH_SHORT)
+                        .show()
+            }
+            return@setOnMenuItemClickListener true
+        }
+
+
+
         getDataApi()
     }
 
-    private fun getDataApi() {
-        val callRomance: Call<RomanceBooks> = BookClient.instance.getRomanceBooks()
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_home, menu)
+
+        val search = menu.findItem(R.id.tb_search)
+        val searchView = search?.actionView as SearchView
+        searchView.isSubmitButtonEnabled = true
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Log.d("textChange", "newText : " + newText)
+                homeAdapter.filter.filter(newText)
+                return false
+            }
+        })
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+
+     private fun getDataApi() {
+        val callRomance: Call<RomanceBooks> = BookClient.instance.getRomanceBooks(
+                0,
+                40
+        )
 
 
         callRomance.enqueue(object : Callback<RomanceBooks> {
@@ -56,20 +94,23 @@ class HomeFragment : Fragment() {
 
                 if (response.isSuccessful) {
                     Log.d("test", response.body()!!.toString())
-                    homeAdapter.setData(response.body()!!.items)
+                    homeAdapter.setData(response.body()!!.items as ArrayList<Item>)
                 }
 
             }
 
             override fun onFailure(call: Call<RomanceBooks>, t: Throwable) {
-                Toast.makeText(requireContext(), "Check your network connection", Toast.LENGTH_LONG).show()
+                Toast.makeText(requireContext(),
+                        "Check your network connection",
+                        Toast.LENGTH_LONG)
+                        .show()
                 Log.e("error", "OnFailure : " + t.message)
             }
 
         })
     }
 
-    private fun setupRecycler() {
 
-    }
+
+
 }
