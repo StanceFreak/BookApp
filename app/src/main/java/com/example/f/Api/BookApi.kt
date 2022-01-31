fe0595b34fc6d3d4471e3b4f6820da713f5c3816
StanceFreak <jombor.army67@gmail.com>
2022-01-31 22:31:51 +0800

    Stuck at nested recyclerview api call

--- app/src/main/java/com/example/f/Api/BookApi.kt
+++ app/src/main/java/com/example/f/Api/BookApi.kt
@@ -1,16 +1,36 @@
 package com.example.f.Api
 
 
-import com.example.f.ModelRomance.RomanceBooks
+import com.example.f.Model.Romance.BooksResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface BookApi {
 
+//    @GET("/books/v1/volumes?q=subject:romance")
+//    suspend fun getRomanceBooks(
+////            @Path("id") id: String,
+//            @Query("startIndex ") startIndex: Int,
+//            @Query("maxResults") maxResults: Int
+//    ): RomanceBooks
+//
+//    @GET("/books/v1/volumes?q=subject:adventure")
+//    suspend fun getAdventureBooks(
+//            @Query("startIndex ") startIndex: Int,
+//            @Query("maxResults") maxResults: Int
+//    ): AdventureBooks
+
     @GET("/books/v1/volumes?q=subject:romance")
     suspend fun getRomanceBooks(
+//            @Path("id") id: String,
             @Query("startIndex ") startIndex: Int,
             @Query("maxResults") maxResults: Int
-    ): RomanceBooks
+    ): BooksResponse
+
+    @GET("/books/v1/volumes?q=subject:adventure")
+    suspend fun getAdventureBooks(
+            @Query("startIndex ") startIndex: Int,
+            @Query("maxResults") maxResults: Int
+    ): BooksResponse
 
 }
--- app/src/main/java/com/example/f/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/f/Fragment/HomeFragment.kt
@@ -6,8 +6,8 @@ import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.GridLayoutManager
-import com.example.f.Adapter.HomeAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.f.Adapter.GroupAdapter
 import com.example.f.Api.BookApiClient
 import com.example.f.Api.BookApiHelper
 import com.example.f.Factory.BookViewModelFactory
@@ -18,7 +18,7 @@ import com.example.f.databinding.FragmentHomeBinding
 
  class HomeFragment : Fragment(){
 
-    private lateinit var homeAdapter: HomeAdapter
+    private lateinit var groupAdapter: GroupAdapter
     private lateinit var binding: FragmentHomeBinding
     private lateinit var apiViewModel: BookApiViewModel
 
@@ -34,6 +34,13 @@ import com.example.f.databinding.FragmentHomeBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+//        val title = GroupTitle.title
+//        val data = ArrayList<Type>()
+//
+//        for (t in title.indices) {
+//            val groupTitle = Type(title[t])
+//            data.add(groupTitle)
+//        }
 
         setupRecycler()
         setupToolbar()
@@ -48,27 +55,32 @@ import com.example.f.databinding.FragmentHomeBinding
         val searchView = search?.actionView as SearchView
         searchView.isSubmitButtonEnabled = true
         searchView.queryHint = "Looking for something?"
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                homeAdapter.filter.filter(newText)
-                val e : Exception = Exception()
-                e.printStackTrace()
-                return true
-            }
-        })
+//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                return false
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                romanceAdapter.filter.filter(newText)
+//                val e : Exception = Exception()
+//                e.printStackTrace()
+//                return true
+//            }
+//        })
         return super.onCreateOptionsMenu(menu, inflater)
     }
 
      private fun setupRecycler() {
-         homeAdapter = HomeAdapter()
 
-         binding.homeRvRomance.apply {
-             layoutManager = GridLayoutManager(requireActivity(), 2)
-             adapter = homeAdapter
+         groupAdapter = GroupAdapter()
+
+         binding.rvHome.apply {
+             layoutManager = LinearLayoutManager(
+                     requireActivity(),
+                     LinearLayoutManager.HORIZONTAL,
+                     false
+             )
+             adapter = groupAdapter
              setHasFixedSize(true)
          }
 
@@ -79,11 +91,11 @@ import com.example.f.databinding.FragmentHomeBinding
          binding.homeToolbar.setOnMenuItemClickListener {
              when (it.itemId) {
                  R.id.search_home -> Toast.makeText(
-                     context,
-                     "Clicked search button",
-                     Toast.LENGTH_SHORT
+                         context,
+                         "Clicked search button",
+                         Toast.LENGTH_SHORT
                  )
-                     .show()
+                         .show()
              }
              return@setOnMenuItemClickListener true
          }
@@ -92,32 +104,58 @@ import com.example.f.databinding.FragmentHomeBinding
      private fun setupObserver() {
 
          apiViewModel = ViewModelProviders.of(
-             this,
-             BookViewModelFactory(BookApiHelper(BookApiClient.instance))
+                 this,
+                 BookViewModelFactory(BookApiHelper(BookApiClient.instance))
          ).get(BookApiViewModel::class.java)
 
-         apiViewModel.getRomanceBooks(
-             0,
-             40
-         ).observe(viewLifecycleOwner, {
-            it?.let { resource ->
-                when(resource.status) {
-                    Status.SUCCESS -> {
-                        binding.progressBar.visibility = View.GONE
-                        binding.homeRvRomance.visibility = View.VISIBLE
-                        resource.data?.let { response ->
-                            homeAdapter.setData(response.items)
-                        }
-                    }
-                    Status.ERROR -> {
-                        binding.progressBar.visibility = View.INVISIBLE
-                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
-                    }
-                    Status.LOADING -> {
-                        binding.progressBar.visibility = View.VISIBLE
-                    }
-                }
-            }
-         })
+         apiViewModel.apply {
+             getRomanceBooks(
+                     0,
+                     40
+             ).observe(viewLifecycleOwner, {
+                 it?.let { resource ->
+                     when (resource.status) {
+                         Status.SUCCESS -> {
+                             binding.progressBar.visibility = View.GONE
+                             binding.rvHome.visibility = View.VISIBLE
+                             resource.data?.let { response ->
+                                 groupAdapter.setData(response)
+                             }
+                         }
+                         Status.ERROR -> {
+                             binding.progressBar.visibility = View.INVISIBLE
+                             Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                         }
+                         Status.LOADING -> {
+                             binding.progressBar.visibility = View.VISIBLE
+                         }
+                     }
+                 }
+             })
+
+             getAdventureBooks(
+                     0,
+                     40
+             ).observe(viewLifecycleOwner, {
+                 it?.let { resource ->
+                     when (resource.status) {
+                         Status.SUCCESS -> {
+                             binding.progressBar.visibility = View.GONE
+                             binding.rvHome.visibility = View.VISIBLE
+                             resource.data?.let { response ->
+                                 groupAdapter.setData(response)
+                             }
+                         }
+                         Status.ERROR -> {
+                             binding.progressBar.visibility = View.INVISIBLE
+                             Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                         }
+                         Status.LOADING -> {
+                             binding.progressBar.visibility = View.VISIBLE
+                         }
+                     }
+                 }
+             })
+         }
      }
 }
